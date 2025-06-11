fdade3366484ab595158ec37cdc19f0ea72a638e
egotting <henricodss@gmail.com>
2025-06-11 13:35:52 -0300

    [Feat]: Impl states type

--- src/main/java/com/github/egotting/BackEnd/Domain/entities/Address/Address.java
+++ src/main/java/com/github/egotting/BackEnd/Domain/entities/Address/Address.java
@@ -2,18 +2,16 @@ package com.github.egotting.BackEnd.Domain.entities.Address;
 
 
 import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.Country;
-import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.State;
+import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.States;
 import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.Street;
 import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.ZipCode;
 import com.github.egotting.BackEnd.Domain.entities.CValueObjects.Identity;
 import com.github.egotting.BackEnd.Domain.entities.Person.Customer.Customer;
 
-import java.util.UUID;
-
 public class Address {
     private Identity id;
     private Street street;
-    private State state;
+    private States state;
     private Country country;
     private ZipCode zpc;
     private Customer user;
