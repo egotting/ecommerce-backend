eec79af8b0bc8fdaddd584d7e153a519de1cc18f
egotting <henricodss@gmail.com>
2025-06-13 12:32:54 -0300

    [Refactor]: Refactor address attributes and put the design pattern builder

--- src/main/java/com/github/egotting/BackEnd/Domain/entities/Address/Address.java
+++ src/main/java/com/github/egotting/BackEnd/Domain/entities/Address/Address.java
@@ -1,16 +1,62 @@
 package com.github.egotting.BackEnd.Domain.entities.Address;
 
 
+import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.City;
 import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.States;
 import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.Street;
 import com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects.ZipCode;
-import com.github.egotting.BackEnd.Domain.entities.CValueObjects.Identity;
 import com.github.egotting.BackEnd.Domain.entities.Person.Customer.Customer;
 
+
 public class Address {
-    private Identity id;
+    public Address(AddressBuilder builder) {
+        this.street = builder.street;
+        this.city = builder.city;
+        this.state = builder.state;
+        this.zpc = builder.zpc;
+        this.user = builder.user;
+    }
+
     private Street street;
+    private City city;
     private States state;
     private ZipCode zpc;
     private Customer user;
+
+    public static class AddressBuilder {
+        private Street street;
+        private City city;
+        private States state;
+        private ZipCode zpc;
+        private Customer user;
+
+        public AddressBuilder street(Street street) {
+            this.street = street;
+            return this;
+        }
+
+        public AddressBuilder city(City city) {
+            this.city = city;
+            return this;
+        }
+
+        public AddressBuilder state(States state) {
+            this.state = state;
+            return this;
+        }
+
+        public AddressBuilder zipCode(ZipCode zpc) {
+            this.zpc = zpc;
+            return this;
+        }
+
+        public AddressBuilder user(Customer user) {
+            this.user = user;
+            return this;
+        }
+
+        public Address build() {
+            return new Address(this);
+        }
+    }
 }
---
+++ src/main/java/com/github/egotting/BackEnd/Domain/entities/Address/ValueObjects/City.java
@@ -0,0 +1,26 @@
+package com.github.egotting.BackEnd.Domain.entities.Address.ValueObjects;
+
+import java.util.Objects;
+
+public class City {
+    private String value;
+
+    public City(String value) {
+        if (value == null || value.isEmpty() || !value.matches("^[a-zA-Z]+(?:(?:\\s+|-)[a-zA-Z]+)*$")) {
+            throw new IllegalArgumentException("City name cannot be null or empty");
+        }
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(value, city.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
