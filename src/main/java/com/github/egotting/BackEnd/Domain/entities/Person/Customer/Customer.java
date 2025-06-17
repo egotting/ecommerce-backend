649b0c3ba20e5ef86cc78b869fabc9b1d589cdbf
egotting <henricodss@gmail.com>
2025-06-17 15:18:51 -0300

    [Feat]: Impl methods get

--- src/main/java/com/github/egotting/BackEnd/Domain/entities/Person/Customer/Customer.java
+++ src/main/java/com/github/egotting/BackEnd/Domain/entities/Person/Customer/Customer.java
@@ -4,8 +4,10 @@ import com.github.egotting.BackEnd.Domain.entities.Orders.Orders;
 import com.github.egotting.BackEnd.Domain.entities.Person.Customer.Objects.ContactCustomer;
 import com.github.egotting.BackEnd.Domain.entities.Person.Customer.Objects.CustomerId;
 import com.github.egotting.BackEnd.Domain.entities.Person.Customer.Objects.InfoCustomer;
+import com.github.egotting.BackEnd.Domain.entities.Person.User.User;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Customer {
 
@@ -21,8 +23,32 @@ public class Customer {
     private ContactCustomer contactInfo;
     private List<Orders> orders;
 
-    public String getCpfValue(){
+    public UUID getId() {
+        return customerId.getId();
+    }
+
+    public User getUser() {
+        return customerId.getUser();
+    }
+
+    public String getFullName() {
+        return infoCustomer.getFullName();
+    }
+
+    public String getAddress() {
+        return infoCustomer.getAddress().toString();
+    }
+
+    public String getCpfValue() {
         return infoCustomer.getCpf();
+    }
+
+    public String getPhoneNumber() {
+        return contactInfo.getPhoneCustomer();
+    }
+
+    public List<Orders> getOrders() {
+        return orders;
     }
 }
 
