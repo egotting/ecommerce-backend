1a185afd75e2f76fb6d00184a85a1c3a7beb23aa
egotting <henricodss@gmail.com>
2025-06-30 15:55:10 -0300

    [FEAT]: Impl repository Roles

---
+++ src/main/java/com/github/egotting/BackEnd/Adapters/out/persistence/repositories/Jpa/Roles/JpaRolesRepository.java
@@ -0,0 +1,7 @@
+package com.github.egotting.BackEnd.Adapters.out.persistence.repositories.Jpa.Roles;
+
+import com.github.egotting.BackEnd.Adapters.out.persistence.entities.Roles.*;
+import org.springframework.data.jpa.repository.*;
+
+public interface JpaRolesRepository extends JpaRepository<JpaRoles, Long> {
+}
