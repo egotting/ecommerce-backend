de014255939dc7e89fd72829ae4e06e80997d36d
egotting <henricodss@gmail.com>
2025-06-30 15:03:36 -0300

    [FEAT]: Impl repository User

--- src/main/java/com/github/egotting/BackEnd/Adapters/out/persistence/repositories/Jpa/Person/User/JpaUserRepository.java
+++ src/main/java/com/github/egotting/BackEnd/Adapters/out/persistence/repositories/Jpa/Person/User/JpaUserRepository.java
@@ -2,8 +2,10 @@ package com.github.egotting.BackEnd.Adapters.out.persistence.repositories.Jpa.Pe
 
 import com.github.egotting.BackEnd.Adapters.out.persistence.entities.Person.User.JpaUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.*;
 
 import java.util.UUID;
 
+@Repository
 public interface JpaUserRepository extends JpaRepository<JpaUser, UUID> {
 }
